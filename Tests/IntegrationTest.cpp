f3334a2037308161bc74bdddd7e93f6b27b14732
Gustice <Jakob.Ger@gmail.com>
2019-09-30 00:19:21 +0200

    CMake-Files although its not working yet

--- Tests/IntegrationTest.cpp
+++ Tests/IntegrationTest.cpp
@@ -12,13 +12,3 @@ TEST_CASE("Just some test to validate process", "[Test]") // To run: UTeval [Con
     REQUIRE(true == true);
 }
 
-TEST_CASE("Just some other pointless test to validate process", "[Test]") // To run: UTeval [Construct]
-{
-    REQUIRE(true == true);
-}
-  
-TEST_CASE("And yet another test to validate process", "[Test]") // To run: UTeval [Construct]
-{
-    REQUIRE(true == true);
-}
-  
