67452398e5e440dc573f7ffc30abd0527a36b31a
Gustice <Jakob.Ger@gmail.com>
2019-10-01 22:41:51 +0200

    DoxyGen documentation moved mostly in cpp-file

--- Source/Modules/Color.cpp
+++ Source/Modules/Color.cpp
@@ -12,38 +12,76 @@
 #include "Color.h"
 #include <string.h>
 
-const Color_t color_Black       = {0x00, 0x00, 0x00, 0x00};
-const Color_t color_Red         = {0xFF, 0x00, 0x00, 0x00};
-const Color_t color_Green       = {0x00, 0xFF, 0x00, 0x00};
-const Color_t color_Blue        = {0x00, 0x00, 0xFF, 0x00};
-const Color_t color_Magenta     = {0x80, 0x00, 0x80, 0x00};
-const Color_t color_Cyan        = {0x00, 0x80, 0x80, 0x00};
-const Color_t color_Yellow      = {0x80, 0x80, 0x00, 0x00};
-const Color_t color_White       = {0x55, 0x55, 0x55, 0x00};
-const Color_t color_WarmWhite   = {0x7F, 0x4F, 0x4F, 0x00};
-const Color_t color_ColdWhite   = {0x4F, 0x4F, 0x7F, 0x00};
-const Color_t color_SpookyWhite = {0x4F, 0x7F, 0x4F, 0x00};
+/** @name Color structure definition
+ *  @details Different predefined colors
+ *  @note The colors are defined that the sum of all channels equals roughly 100 %,
+ *    so that a Red should appear equally bright to white (where all channels are switched on,
+ *    but with 33% at each channel).
+ *    However the efficiency of each channel is not considered (maybe in derived classes or driver layer)
+ */
+///@{
+const Color_t color_Black       = {0x00, 0x00, 0x00, 0x00}; ///< Black color structure constant
+const Color_t color_Red         = {0xFF, 0x00, 0x00, 0x00}; ///< Red color structure constant
+const Color_t color_Green       = {0x00, 0xFF, 0x00, 0x00}; ///< Green color structure constant
+const Color_t color_Blue        = {0x00, 0x00, 0xFF, 0x00}; ///< Blue color structure constant
+const Color_t color_Magenta     = {0x80, 0x00, 0x80, 0x00}; ///< Magenta color structure constant
+const Color_t color_Cyan        = {0x00, 0x80, 0x80, 0x00}; ///< Cyan color structure constant
+const Color_t color_Yellow      = {0x80, 0x80, 0x00, 0x00}; ///< Yellow color structure constant
+const Color_t color_White       = {0x55, 0x55, 0x55, 0x00}; ///< White color structure constant
+const Color_t color_WarmWhite   = {0x7F, 0x4F, 0x4F, 0x00}; ///< Warm white color structure constant (with higher red channel value)
+const Color_t color_ColdWhite   = {0x4F, 0x4F, 0x7F, 0x00}; ///< Black color structure constant (with higher blue channel value)
+const Color_t color_SpookyWhite = {0x4F, 0x7F, 0x4F, 0x00}; ///< Black color structure constant (with higher green channel value)
 
-const Color _ctBlack(color_Black);
-const Color _ctRed(color_Red);
-const Color _ctGreen(color_Green);
-const Color _ctBlue(color_Blue);
-const Color _ctMagenta(color_Magenta);
-const Color _ctCyan(color_Cyan);
-const Color _ctYellow(color_Yellow);
-const Color _ctWhite(color_White);
-const Color _ctWarmWhite(color_WarmWhite);
-const Color _ctColdWhite(color_ColdWhite);
-const Color _ctSpookyWhite(color_SpookyWhite);
+const Color _ctBlack(color_Black);             ///< Black color object constant
+const Color _ctRed(color_Red);                 ///< Red color object constant
+const Color _ctGreen(color_Green);             ///< Green color object constant
+const Color _ctBlue(color_Blue);               ///< Blue color object constant
+const Color _ctMagenta(color_Magenta);         ///< Magenta color object constant
+const Color _ctCyan(color_Cyan);               ///< Cyan color object constant
+const Color _ctYellow(color_Yellow);           ///< Yellow color object constant
+const Color _ctWhite(color_White);             ///< White color object constant
+const Color _ctWarmWhite(color_WarmWhite);     ///< Warm white color object constant (with higher red channel value)
+const Color _ctColdWhite(color_ColdWhite);     ///< Black color object constant (with higher blue channel value)
+const Color _ctSpookyWhite(color_SpookyWhite); ///< Black color object constant (with higher green channel value)
+///@}
 
+/**
+ * @brief   Construct a new Color object
+ * @details Default color will be Black \ref _ctBlack
+ */
 Color::Color(void) { SetColor(0, 0, 0, 0); }
 
+/**
+ * @brief   Construct a new Color object according to rgb color-channel-values
+ * @details This constructor also takes an overlaping white channel
+ * @param r Red channel
+ * @param g Green channel
+ * @param b Blue channel
+ * @param w White channel (zero by default if not assigned)
+ */
 Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) { SetColor(r, g, b, w); }
 
+/**
+ * @brief Construct a new Color object according to Color_t Array
+ * @param color
+ */
 Color::Color(Color_t color) { memcpy(&_color, &color, sizeof(Color_t)); }
 
+/**
+ * @brief Set the Color object according to Color_t Array
+ *
+ * @param color
+ */
 void Color::SetColor(Color_t color) { memcpy(&_color, &color, sizeof(Color_t)); }
 
+/**
+ * @brief Set the Color object according to rgb color-channel values
+ *
+ * @param r Red channel
+ * @param g Green channel
+ * @param b Blue channel
+ * @param w White channel (zero by default if not assigned)
+ */
 void Color::SetColor(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
     _color.red   = r;
     _color.green = g;
@@ -51,8 +89,18 @@ void Color::SetColor(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
     _color.white = w;
 }
 
+/**
+ * @brief Returns Color as Color_t structure
+ *
+ * @return Color_t Color
+ */
 Color_t Color::GetColor(void) { return _color; }
 
+/**
+ * @brief Writes Color as formatted bytestream to location
+ *
+ * @param color
+ */
 void Color::WriteByteStreamTo(uint8_t raw[sizeof(Color_t)]) {
     raw[0] = _color.blue;
     raw[1] = _color.green;
@@ -60,6 +108,16 @@ void Color::WriteByteStreamTo(uint8_t raw[sizeof(Color_t)]) {
     raw[3] = _color.white;
 }
 
+/**
+ * @brief Overlaying to colors my adding each color channel value
+ *
+ * @note Colors channels should not overlap since there is no automatic ratio correction.
+ *    Use *-Operator first in order to scale each color value a a way that no overflow can occur
+ *
+ * @param c1 Color 1
+ * @param c2 Color 2
+ * @return Overlayed Color
+ */
 Color operator*(Color c1, uint8_t k) {
     c1._color.red   = (uint8_t)((uint16_t)c1._color.red * k / 0xFF);
     c1._color.green = (uint8_t)((uint16_t)c1._color.green * k / 0xFF);
@@ -68,6 +126,13 @@ Color operator*(Color c1, uint8_t k) {
     return c1;
 }
 
+/**
+ * @brief Scale color by scaling each color-channel by given scale-factor
+ *
+ * @param c1 Color
+ * @param k Scale-factor uint8_t.Max is fullscale
+ * @return Scaled Color
+ */
 Color operator+(Color c1, Color c2) {
     c1._color.red   = c1._color.red + c2._color.red;
     c1._color.green = c1._color.green + c2._color.green;
--- Source/Modules/Color.h
+++ Source/Modules/Color.h
@@ -1,8 +1,8 @@
 /**
  * @file Color.h
  * @author Gustice
- * @brief	Color-Class and color Definitions
- * @details	Defintion of Color Class with Predefined Colors as static ressource
+ * @brief   Color-Class and color Definitions
+ * @details Defintion of Color Class with Predefined Colors as static ressource
  *
  * @version 0.1
  * @date 2019-09-30
@@ -13,6 +13,10 @@
 
 #include <stdint.h>
 
+/**
+ * @brief Color Structure 
+ * @details Defines red/green/blue and white color channel values
+ */
 typedef struct Color_def {
     uint8_t red;
     uint8_t green;
@@ -26,102 +30,42 @@ typedef struct Color_def {
 #define USEOLD_COLOR_OBJ NO_COLOR_OBJ ///< Alternative use old color agreement
 
 /**
- * @brief 	Color class
+ * @brief   Color class
  *
  * @details Defines color data and common operations with color objects
- * 	\li Supports RGB with overlapping white channel (e.g. SK6812-LED)
- * 	\li Supprts Operator overloading with '+' and '*'
- * 	\li Return formatted data stream that can be directly sent to an LED
+ *  \li Supports RGB with overlapping white channel (e.g. SK6812-LED)
+ *  \li Supprts Operator overloading with '+' and '*'
+ *  \li Return formatted data stream that can be directly sent to an LED
  */
 class Color {
     friend Color operator+(Color c1, Color c2);
     friend Color operator*(Color c1, uint8_t k);
 
   public:
-    /**
-     * @brief 	Construct a new Color object
-     * @details	Default color will be Black \ref _ctBlack
-     */
     Color(void);
-
-    /**
-     * @brief 	Construct a new Color object according to rgb color-channel-values
-     * @details	This constructor also takes an overlaping white channel
-     * @param r Red channel
-     * @param g Green channel
-     * @param b Blue channel
-     * @param w White channel (zero by default if not assigned)
-     */
     Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w = 0);
-
-    /**
-     * @brief Construct a new Color object according to Color_t Array
-     * @param color
-     */
     Color(Color_t color);
 
-    /**
-     * @brief Set the Color object according to Color_t Array
-     *
-     * @param color
-     */
     void SetColor(Color_t color);
-
-    /**
-     * @brief Set the Color object according to rgb color-channel values
-     *
-     * @param r Red channel
-     * @param g Green channel
-     * @param b Blue channel
-     * @param w White channel (zero by default if not assigned)
-     */
     void SetColor(uint8_t r, uint8_t g, uint8_t b, uint8_t w = 0);
 
-    /**
-     * @brief Returns Color as Color_t structure
-     *
-     * @return Color_t Color
-     */
     Color_t GetColor(void);
-
-    /**
-     * @brief Writes Color as formatted bytestream to location
-     *
-     * @param color
-     */
     void WriteByteStreamTo(uint8_t raw[sizeof(Color_t)]);
 
   private:
     Color_t _color; ///< Color data
 };
 
-/**
- * @brief Overlaying to colors my adding each color channel value
- *
- * @note Colors channels should not overlap since there is no automatic ratio correction.
- * 		Use *-Operator first in order to scale each color value a a way that no overflow can occur
- *
- * @param c1 Color 1
- * @param c2 Color 2
- * @return Overlayed Color
- */
-Color operator+(Color c1, Color c2);
 
-/**
- * @brief Scale color by scaling each color-channel by given scale-factor
- *
- * @param c1 Color
- * @param k Scale-factor uint8_t.Max is fullscale
- * @return Scaled Color
- */
+Color operator+(Color c1, Color c2);
 Color operator*(Color c1, uint8_t k);
 
 /** @name Color structure definition
  *  @details Different predefined colors
  *  @note The colors are defined that the sum of all channels equals roughly 100 %,
- * 		so that a Red should appear equally bright to white (where all channels are switched on,
- * 		but with 33% at each channel).
- * 		However the efficiency of each channel is not considered (maybe in derived classes or driver layer)
+ *    so that a Red should appear equally bright to white (where all channels are switched on,
+ *    but with 33% at each channel).
+ *    However the efficiency of each channel is not considered (maybe in derived classes or driver layer)
  */
 ///@{
 extern const Color_t color_Black;       ///< Black color structure constant
